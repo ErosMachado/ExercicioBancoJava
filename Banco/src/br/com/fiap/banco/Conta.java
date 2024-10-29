e9314200929498cdf12e0a031b9b2006cf2f7a7b
soabarros <sophiasoaresbarros397@gmail.com>
2024-10-29 17:49:16 -0300

    executavel

--- Banco/src/br/com/fiap/banco/Conta.java
+++ Banco/src/br/com/fiap/banco/Conta.java
@@ -1,18 +1,57 @@
 package br.com.fiap.banco;
 
 public class Conta {
-	String nomeCliente;
-	String cpfCliente;
-	int numeroConta;
-	double saldo;
+	private String nomeCliente;
+	private String cpfCliente;
+	private int numeroConta;
+	private double saldo;
 
-	public Conta(String nomeCliente, String cpfCliente, int numeroConta, double saldoInicial) {
+	public Conta(String nomeCliente, String cpfCliente, int numeroConta, double saldo) {
+		super();
 		this.nomeCliente = nomeCliente;
 		this.cpfCliente = cpfCliente;
 		this.numeroConta = numeroConta;
-		this.saldo = saldoInicial;
+		this.saldo = saldo;
+	}
+	// getters / stters ctrl 3
+
+	public String getNomeCliente() {
+		return nomeCliente;
 	}
 
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	/*
+	 * public Conta(String nomeCliente, String cpfCliente, int numeroConta, double
+	 * saldoInicial) { this.nomeCliente = nomeCliente; this.cpfCliente = cpfCliente;
+	 * this.numeroConta = numeroConta; this.saldo = saldoInicial; }
+	 */
 	public boolean sacar(double valor) {
 		if (saldo >= valor) {
 			saldo -= valor;
@@ -37,7 +76,7 @@ public class Conta {
 
 	public void exibirSaldo() {
 		System.out.println("Nome: " + nomeCliente);
-		System.out.println("Saldo: R$ "+ saldo);
+		System.out.println("Saldo: R$ " + saldo);
 		System.out.println("----------------");
 	}//
 }
