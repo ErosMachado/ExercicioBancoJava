18d367b0bf5f820e1c2cd2cd04039864148b201a
erosmachado <erosrodriguesmachado@gmail.com>
2024-11-06 17:20:22 -0300

    modificações na conta corrente

--- Banco/src/br/com/fiap/banco/SaldoInsuficiente.java
+++ Banco/src/br/com/fiap/banco/SaldoInsuficiente.java
@@ -2,6 +2,11 @@ package br.com.fiap.banco;
 
 public class SaldoInsuficiente extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	SaldoInsuficiente(String mensagem) {
 		super(mensagem);
 	}
--- Banco/src/executaveis/CriarConta.java
+++ Banco/src/executaveis/CriarConta.java
@@ -2,7 +2,7 @@ package executaveis;
 
 import br.com.fiap.banco.Cliente;
 import br.com.fiap.banco.ContaCorrente;
-import br.com.fiap.banco.*;
+//import br.com.fiap.banco.*;
 
 public class CriarConta {
 
@@ -10,18 +10,18 @@ public class CriarConta {
 
 		Cliente cliente1 = new Cliente("Eduardo Sousa", "edua@gmail.com", "133.365.453/32", "(12) 99831-9649", 1002,
 				10000.00, "10/10/2003");
-		
+		/*
 		CDB cdb = new CDB();
 		LCA lca = new LCA();
-		LCI lci = new LCI();
+		LCI lci = new LCI();*/
 		
 		ContaCorrente cliente11 = new ContaCorrente(cliente1, 200000);
 		cliente11.depositar(10);
 		cliente11.consultarSaldo();
-
+/*
 		cliente11.investimento(lci, 100000);
 		cliente11.investimento(cdb, 100000);
 		cliente11.investimento(lca, 100000);
-		cliente11.exibirSaldo();
+		cliente11.exibirSaldo();*/
 	}
 }
